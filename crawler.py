d3503ee5428208b8a7db064c645eaae2e4884289
Alexis Belmonte <alexbelm48@gmail.com>
2025-03-14 16:42:20 +0100

    crawler: download process implemented for all repositories

--- crawler.py
+++ crawler.py
@@ -47,6 +47,7 @@ class Crawler:
 
             repo_local_path = os.path.join(self.working_directory.name, repo.name)
             L.debug("Local path for repository is {}", repo_local_path)
+            os.makedirs(repo_local_path)
 
             repo_tarball_url = repo.get_archive_link("tarball")
             
@@ -54,4 +55,15 @@ class Crawler:
             
             with requests.get(repo_tarball_url, stream=True) as tarball_stream, \
                  tarfile.open(fileobj=io.BytesIO(tarball_stream.raw.read()), mode="r:gz") as tarball:
-                    L.debug("Tarball has {} elements", len(tarball.getmembers()))
+                     root_folder_name = tarball.getmember(tarball.getnames()[0]).name
+                     L.trace("Root folder name is {}", root_folder_name)
+
+                     for member in tarball.getmembers():
+                         initial_path = member.name
+                         member.name = member.name.replace(root_folder_name, '', 1)[1:]
+                         target_path = os.path.join(repo_local_path, member.name)
+
+                         L.trace("Extracting member {} -> {}", initial_path, target_path)
+                         tarball.extract(member, path=repo_local_path)
+
+        L.info("Done crawling through all repositories, preparing to upload to target repository")
